937da2ad61218f4faad0cf0df3c348a9af2db12e
joas-barros <joas.barros14@hotmail.com>
2026-01-15 22:57:17 -0300

    first commit

---
+++ Microservices/Delevery-Tracking/src/main/java/com/algaworks/algadelivery/delevery/tracking/DeleveryTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algadelivery.delevery.tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeleveryTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeleveryTrackingApplication.class, args);
+	}
+
+}
---
+++ Microservices/Delevery-Tracking/src/test/java/com/algaworks/algadelivery/delevery/tracking/DeleveryTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.algadelivery.delevery.tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeleveryTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
