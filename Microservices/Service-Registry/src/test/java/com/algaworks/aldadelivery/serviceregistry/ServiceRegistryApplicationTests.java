7c0ac1a0d692c7c9aab5d028be47f6d8c1b48268
joas-barros <joas.barros14@hotmail.com>
2026-01-18 10:01:06 -0300

    feat: config eureka

---
+++ Microservices/Service-Registry/src/test/java/com/algaworks/aldadelivery/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.aldadelivery.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
